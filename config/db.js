0fc5aacc8ffc793264fd6234d84df0fc6b4e33c5
Shahid Gull <parrayshahid97@gmail.com>
2025-01-10 12:07:32 +0530

    added poolsize to db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     console.log("db is connecting...");
-    const res = await mongoose.connect(process.env.mongoURI);
+    const res = await mongoose.connect(process.env.mongoURI, { poolSize: 10 });
     console.log(`mongodb connected with server ${res.connection.host}`);
   } catch (error) {
     console.log("mongodb connection failed!");
