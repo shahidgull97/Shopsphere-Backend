9a7557a4cfa014420496c5def853a6d95cec9d69
Shahid Gull <parrayshahid97@gmail.com>
2025-01-10 11:36:08 +0530

    set cookie to samesite none

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,8 +4,8 @@ export const sendToken = async (user, res, statusCode) => {
   const token = user.getJWTToken();
   const cookieOptions = {
     httpOnly: true,
-    // secure: false, // Set to true in production with HTTPS
-    // sameSite: "None", // Must be 'None' for cross-origin cookies
+    secure: false, // Set to true in production with HTTPS
+    sameSite: "None", // Must be 'None' for cross-origin cookies
 
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
